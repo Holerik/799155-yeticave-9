1819ab3f282e40d115ec783f466c1ff92587c0ff
Alex <a_zobnin@mail.ru>
2019-04-23 22:07:53 +0300

    Работа с датами и ошибками

---
+++ addfunc.php
@@ -0,0 +1,28 @@
+<?php
+function remained_time() {
+    $retVal = [];
+    $cur_date = date_parse(date('H:i:s d.m.Y'));
+    $hour = $cur_date['hour'];
+    $minute = $cur_date['minute'];
+    $maxHour = 24;
+    if ($minute > 0) {
+        $maxHour--;
+        $minute = 60 - $minute;
+    }
+    $hour = $maxHour - $hour;
+    $retVal = [$hour, $minute];
+    return $retVal;
+}
+
+function format_price($price) {
+    $result = "";
+    $price = ceil($price);
+    if ($price > 1000) {
+        $result = number_format($price, 0, ".", " ");
+    }
+    else {
+        $result = $price;
+    }
+    $result .= " ₽";
+    return $result;
+}
