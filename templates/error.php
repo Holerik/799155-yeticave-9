73303036f38a4928e4df09bb0241176a3e49be0a
Alex <a_zobnin@mail.ru>
2019-05-10 11:55:58 +0300

    Небольшая корректировка ошибок

--- templates/error.php
+++ templates/error.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title><?=$title;?></title>
+    <link href="../css/normalize.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
 <main class="error-container">
     <div class="error-container__main-col">
     <header class="error-container__header">
@@ -8,3 +17,5 @@
     </article>
     </div>
 </main>
+</body>
+</html>
