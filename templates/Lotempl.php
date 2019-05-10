0afe88b4f885b45ec89ee954b729e5e72ba2c57e
Alex <a_zobnin@mail.ru>
2019-05-10 20:40:07 +0300

    Исправление ссылок

--- templates/Lotempl.php
+++ templates/Lotempl.php
@@ -33,10 +33,10 @@
       <nav class="user-menu">
         <ul class="user-menu__list">
           <li class="user-menu__item">
-            <a href="sign-up.html">Регистрация</a>
+            <a href="sign-up.php">Регистрация</a>
           </li>
           <li class="user-menu__item">
-            <a href="login.html">Вход</a>
+            <a href="login.php">Вход</a>
           </li>
         </ul>
       </nav>
